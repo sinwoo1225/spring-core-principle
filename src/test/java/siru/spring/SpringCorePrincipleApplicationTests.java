b8fb1630fc47d6e05e5a09190407a1ff79ea754a
shinwoo <sinwoo1225@gmail.com>
2021-11-28 21:28:53 +0900

    프로젝트 초기세팅

---
+++ src/test/java/siru/spring/SpringCorePrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package siru.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrincipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
