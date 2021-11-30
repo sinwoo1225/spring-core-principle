8cf1891113dda2f8c9baa6811591f4a24a09bf6b
shinwoo <sinwoo1225@gmail.com>
2021-11-30 15:35:36 +0900

    회원도매인 개발
    - oop 준수 x 추후 개선

---
+++ src/main/java/siru/spring/MemberApp.java
@@ -0,0 +1,19 @@
+package siru.spring;
+
+import siru.spring.member.Grade;
+import siru.spring.member.Member;
+import siru.spring.member.MemberService;
+import siru.spring.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "A", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
---
+++ src/main/java/siru/spring/member/Grade.java
@@ -0,0 +1,6 @@
+package siru.spring.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
